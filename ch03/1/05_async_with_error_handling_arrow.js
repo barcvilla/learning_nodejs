363cfa6b344ed7c9820a750b3b41976ae66cc52d
Carlos Villanueva <ceva_19@hotmail.com>
2018-03-09 19:45:51 -0300

    async with error handling narow

--- ch03/1/05_async_with_error_handling_arrow.js
+++ ch03/1/05_async_with_error_handling_arrow.js
@@ -3,10 +3,21 @@ var fs = require('fs');
    se haya invocado a la funcion callback*/
 fs.open('info.txt', 'r', (err, handle) =>
 {
+    if(err)
+    {
+        console.log("ERROR: " + err.code + "(" + err.message + ")");
+        return;
+    }
+
     var buf = new Buffer(100000);    
     
     fs.read(handle, buf, 0, 100000, null, (err, length) =>
     {
+        if(err)
+        {
+            console.log("ERROR: " + err.code + "(" + err.message + ")");
+            return;
+        }
         console.log(buf.toString('utf-8', 0, length));
         fs.close(handle, () => {/*dont care*/});
     });
